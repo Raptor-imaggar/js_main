0e092756cdd819c0d2475207812d05514e44288c
chandru2022csi <121219892+chandru2022csi@users.noreply.github.com>
2024-02-07 18:18:30 +0530

    replace

--- src/pages/Slacesite.js
+++ src/pages/Slacesite.js
@@ -1,21 +1,24 @@
 import React from "react";
-
-
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import sitesData from "../data/sitesData";
-const cardInfo = sitesData
+const cardInfo = sitesData;
 
 export default function Slacesite() {
- const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <div className="bg-white py-12 lg:py-24">
       <div className="container mx-auto my-8 px-4 lg:px-20" data-aos="zoom-in">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
           {cardInfo.map((card, index) => (
-           
-            <div key={index} onClick={() => navigate(`/details/${index}`)}> {/* Use navigate */} 
+            <div key={index} onClick={() => navigate(`/details/${index}`)}>
+              {" "}
+              {/* Use navigate */}
               <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img className="w-full" src={card.image} alt={card.location} />
+                <img
+                  className="w-full h-48 object-cover "
+                  src={card.image}
+                  alt={card.location}
+                />
                 <div className="px-6 py-4">
                   <div className="font-bold text-xl mb-2">{card.location}</div>
                   <p className="text-gray-700 text-base">{card.version}</p>
