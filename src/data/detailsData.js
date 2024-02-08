c420649a1b671b36e4880416023b9a75987f9d16
21rabi2003 <121427757+Rabijackson2022csi@users.noreply.github.com>
2024-02-08 19:17:22 +0530

    created detailsdata js file

---
+++ src/data/detailsData.js
@@ -0,0 +1,92 @@
+const detailsData = {
+    gried:{
+        image1:"/images/fo-img.jpg",
+        image2:"/images/fo-img.jpg",
+        image3:"/images/fo-img.jpg",
+        image4:"/images/fo-img.jpg",
+        image5:"/images/fo-img.jpg"
+    },
+    land:{
+        title:" bungalow in Jog Falls",
+        sub_title:" Located near Thanjavur"
+    },
+    highlights:{
+        icon:"",
+        title:"Dive right in",
+        discription:"his is one of the few places in the area with a pool."
+    },
+    about:{
+        ab_title:"Jayam structures",
+        ab_sub_title:"Experienced host",
+        ab_discription:"Jayam has 31 reviews for other places."
+    },
+    reviews:{
+        review1:{
+            name:"Divyesh",
+            date:"November 2023",
+            image:"/images/fo-img.jpg",
+            content:"Very nice stay, especially for group of 4 to 8 ppl. Pool maintenance is good Washroom is clean"
+        },
+        review2:{
+            name:"Siddharth",
+            date:"May 2023",
+            image:"/images/fo-img.jpg",
+            content:"One of bathrooms was bot attached to room. This was bot highlighted in the pictures. The road reaching the exact location needs to be made. During rains this can lead to accidents if driver / car are not experienced. Great restaurant and location by the Kaveri river."
+        },
+        review3:{
+            name:"Chethan",
+            date:"April 2023",
+            image:"/images/fo-img.jpg",
+            content:"The actual loaction of the place and the location displaying on Airbnb is completly different (around 20km). othere than this there is no issue with the stay."
+        },
+        review4:{
+            name:"Umesh",
+            date:"March 2023",
+            image:"/images/fo-img.jpg",
+            content:""
+        },
+        review5:{
+            name:"Antony",
+            date:"March 2023",
+            image:"/images/fo-img.jpg",
+            content:"If you want peace of mind, stay away from Airbnb and any property listed on it. "
+        }
+    },
+    thingstoknow:{
+        th_row1:{
+            sub_title:"Rules",
+            content1:"Rental Agreement",
+            content2:"Insurance Requirements",
+            content3:"Respect for Neighbors"
+        },
+        th_row2:{
+            sub_title:"Safety & property",
+            content1:"EC verified properties ",
+            content2:"Security Protocols"
+        },
+        th_row3:{
+            sub_title:"Cancellation policy",
+            content1:"Users must provide a specified amount of notice.",
+            content2:"Users must communicate their intention to cancel the lease or rental agreement to the landlord."
+        },
+    },
+    amenities:{
+        title:"Amenities",
+        content1:"Fitness Center",
+        content2:"Swimming Pool",
+        content3:"Clubhouse",
+        content4:"Business Center",
+        content5:"Pet-Friendly Facilities",
+        content6:"On-Site Parking",
+        content7:"Landscaping and Green Spaces",
+    },
+    tovisit:{
+        price:"2,00,000",
+        own_by:"Chan",
+        ec:"Verified",
+        location:"Thanjavur",
+        bond_value:"20,000"
+    }
+
+    }
+export default  detailsData;
